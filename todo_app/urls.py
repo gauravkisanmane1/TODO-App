e65fa2c1585724d5343815680ab9f36c3d7e0032
gauravkisanmane1 <87034038+gauravkisanmane1@users.noreply.github.com>
2022-02-09 23:38:43 +0530

    todo app
    simple todo app using python django

---
+++ todo_app/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path, include
+from .import views
+
+urlpatterns = [
+        path('', views.index, name='index'),
+    path('delete/<id>', views.deletetodo, name='delete'),
+    path('mark/<id>', views.marktodo, name='marktodo'),
+    path('unmark/<id>', views.unmarkedtodo, name='unmarktodo'),
+    path('add', views.addtodo, name='addtodo'),
+
+]
